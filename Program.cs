3b1eb0138bd250de5bcdde7e50abdb22c2d6ff4c
sazid-bytes <sazid.sunny@gmail.com>
2022-04-23 08:59:44 +0530

    Branch2 Added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Sazid!");
+Console.WriteLine("Hello Raiz!");
 Console.WriteLine("Sazid");
 Console.WriteLine("Raiz");
